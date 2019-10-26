8afc4df1fffd1c904ea22be51ceead281c87dce9
Ivan Lavoryk <lavoryk@gmail.com>
2019-10-26 15:05:01 +0300

    Tidy up

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <Arduino.h>
-#include <WiFi.h>
 
 uint64_t chipid;
 void setup() 
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   delay(10);
   
@@ -33,7 +31,6 @@ void setup()
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
   chipid=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
   Serial.printf("ESP32 Chip ID = %04X",(uint16_t)(chipid>>32));//print High 2 bytes
   Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
